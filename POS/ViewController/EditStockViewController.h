61bf82bd99f014dbd7e21d5db339aa72e80b39b5
Adisak Phairat <boy-28270@hotmail.com>
2019-12-01 21:16:48 +0700

    add view edit stock

---
+++ POS/ViewController/EditStockViewController.h
@@ -0,0 +1,17 @@
+//
+//  EditStockViewController.h
+//  POS
+//
+//  Created by Adisak Phairat on 1/12/2562 BE.
+//  Copyright © 2562 Adisak Phairat. All rights reserved.
+//
+
+#import "ViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface EditStockViewController : ViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
